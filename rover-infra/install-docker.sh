cca24e578d122286f80015a31cb401519b4eb18c
Vadym S. Khondar <vadym@khondar.name>
2019-01-04 00:51:15 +0000

    Add docker install script.

---
+++ rover-infra/install-docker.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+curl -sSL https://get.docker.com | sh
+curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
