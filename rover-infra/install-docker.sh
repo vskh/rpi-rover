4fa22d8768f4753c1e959d8c96eac2d835b0f4c9
Vadym S. Khondar <vadym@khondar.name>
2019-12-08 02:05:08 +0000

    Parametrize deployed image tag.
    Also, fix docker-compose install.

--- rover-infra/install-docker.sh
+++ rover-infra/install-docker.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 curl -sSL https://get.docker.com | sh
-curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+apt install python3-pip
+
+# end
