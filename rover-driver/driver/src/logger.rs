e9fd71fe3cb71fdeaca500cf052e071edf823cdb
Vadym S. Khondar <vadym@khondar.name>
2019-12-31 20:53:38 +0000

    Add logging.

---
+++ rover-driver/driver/src/logger.rs
@@ -0,0 +1,21 @@
+use log4rs;
+use log4rs::config::{Config, Root, Appender};
+use log4rs::append::console::ConsoleAppender;
+
+pub fn init_log(config_path: Option<String>) -> Result<(), Box<dyn std::error::Error>> {
+    if let Some(path) = config_path {
+        log4rs::init_file(path, Default::default())
+            .map_err(|e| format!("Log initialization error: {}", e))?;
+        Ok(())
+    } else {
+        let console_appender = ConsoleAppender::builder().build();
+        let config = Config::builder()
+            .appender(Appender::builder().build("console", Box::new(console_appender)))
+            .build(Root::builder()
+                .appender("console")
+                .build(log::LevelFilter::Info))?;
+        log4rs::init_config(config)
+            .map_err(|e| format!("Log initialization error: {}", e))?;
+        Ok(())
+    }
+}
--- rover-driver/driver/src/main.rs
+++ rover-driver/driver/src/main.rs
@@ -1,15 +1,32 @@
+mod logger;
+
+use log::{trace, debug, info, warn, error};
 use tokio::net::TcpListener;
 use tokio::prelude::*;
+
+const CONFIG_FILE: &str = "./Config";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // load settings
     let mut settings = config::Config::default();
     settings
-        .merge(config::File::with_name("Config"))?;
+        .merge(config::File::with_name(CONFIG_FILE))?;
+
+    // initialize logging
+    logger::init_log(settings.get_str("log_config").ok())?;
 
     // bind socket
     let mut listener = TcpListener::bind(settings.get_str("listen")?).await?;
+
+    // start accepting connections
+    loop {
+        let (mut socket, client_addr) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            debug!("Received connection from {}.", client_addr);
+        });
+    }
 
     Ok(())
 }
