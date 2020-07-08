5fc2d84dfda2375f05350f11f307bf48a9d80df1
Vadym S. Khondar <vadym@khondar.name>
2020-07-08 20:09:29 +0100

    Refactor ridecontrol to support both local and remote mode.

--- rover-firmware/ridecontrol/src/main.rs
+++ rover-firmware/ridecontrol/src/main.rs
@@ -1,15 +1,28 @@
+use clap::{clap_app, crate_authors, crate_description, crate_version};
+
+use libapi_net::client::Client;
 use libdriver_robohat::RobohatRover;
 use libridecontrol::controller::RideController;
 
-fn main() {
-    match RobohatRover::new() {
-        Err(e) => println!("Rover initialization error: {}", e),
-        Ok(rover) => match RideController::new(rover) {
-            Err(e) => println!("Ride controller initialization error: {}", e),
-            Ok(mut controller) => match controller.run() {
-                Err(e) => println!("Rover issue: {}", e),
-                Ok(()) => println!("Done.")
-            }
-        }
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let opts = clap_app!(RoverRideController =>
+        (version: crate_version!())
+        (author: crate_authors!("\n"))
+        (about: crate_description!())
+        (@group mode +required =>
+            (@arg local: -l --local "Enable local mode (if running directly on rover)")
+            (@arg address: -r --remote +takes_value "Enable remote mode (if connecting through net API)")
+        )
+    ).get_matches();
+
+    if opts.is_present("local") {
+        RideController::new(RobohatRover::new()?)?.run()?
+    } else {
+        let rover_address = opts.value_of("address").unwrap();
+
+        RideController::new(Client::new(rover_address).await?)?.run()?
     }
+
+    Ok(())
 }
