98357b582adf0fa036d245336871d96fec3ccc0c
Vadym S. Khondar <vadym@khondar.name>
2021-01-24 18:34:23 +0000

    Fix startup script.

--- rover-infra/video/run.sh
+++ rover-infra/video/run.sh
@@ -4,6 +4,6 @@ chown vlc:video /dev/video0
 
 /usr/bin/sudo -u vlc cvlc v4l2:///dev/video0 \
   --v4l2-width 1920 --v4l2-height 1080 --v4l2-chroma h264 \
-  --sout '#standard{access=httpmux=tsdst=0.0.0.0:5858}' -vvv
+  --sout '#standard{access=http,mux=ts,dst=0.0.0.0:5858}' -vvv
 
 # end
