b593f7d3f53c355048783510593d9003a451a1bf
Vadym S. Khondar <vadym@khondar.name>
2024-02-28 23:18:53 +0000

    Upgrade and fix upgrade issues in ux-console.

--- rover-firmware/ux-console/src/main.rs
+++ rover-firmware/ux-console/src/main.rs
@@ -1,4 +1,4 @@
-use clap::{clap_app, crate_authors, crate_description, crate_version};
+use clap::{arg, command, value_parser, ArgAction, ArgGroup};
 
 use libapi_net::client::Client;
 use libdriver::util::a_sync::AsyncRover;
@@ -7,21 +7,27 @@ use libux_console::controller::RideController;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let opts = clap_app!(RoverRideController =>
-        (version: crate_version!())
-        (author: crate_authors!("\n"))
-        (about: crate_description!())
-        (@group mode +required =>
-            (@arg local: -l --local "Enable local mode (if running directly on rover)")
-            (@arg address: -r --remote +takes_value "Enable remote mode (if connecting through net API)")
+    let opts = command!()
+        .arg(
+            arg!(local: -l --local "Enable local mode (if running directly on the rover)")
+                .action(ArgAction::SetTrue),
         )
-    ).get_matches();
+        .arg(
+            arg!(address: -r --remote <ADDR> "Enable remote mode (if connecting through net API)")
+                .value_parser(value_parser!(String)),
+        )
+        .group(
+            ArgGroup::new("mode")
+                .args(["local", "remote"])
+                .required(true),
+        )
+        .get_matches();
 
-    if opts.is_present("local") {
+    if opts.contains_id("local") {
         let async_rover: AsyncRover<RobohatRover> = RobohatRover::new()?.into();
         RideController::new(async_rover)?.run().await?
     } else {
-        let rover_address = opts.value_of("address").unwrap();
+        let rover_address = opts.get_one::<String>("address").unwrap();
 
         RideController::new(Client::new(rover_address).await?)?
             .run()
